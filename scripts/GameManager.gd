22473d87a9211aaf907cd50253d1541354cc7f8e
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2024-05-27 15:49:44 +0100

    Add sounds to unit selection

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,7 +2,10 @@ extends Node
 class_name GameManager
 
 @export var units: Array[Unit] = []
+
 @onready var camera: Camera = %Camera
+@onready var select_sound = $SelectSound
+
 var selected_unit = 0
 
 
@@ -14,8 +17,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("select_next_unit"):
-		selected_unit = wrap(selected_unit + 1, 0, len(units))
-		camera.target = units[selected_unit]
+		select_unit(1)
 	elif Input.is_action_just_pressed("select_prev_unit"):
-		selected_unit = wrap(selected_unit - 1, 0, len(units))
-		camera.target = units[selected_unit]
+		select_unit(-1)
+
+
+func select_unit(direction: int):
+	selected_unit = wrap(selected_unit + direction, 0, len(units))
+	camera.target = units[selected_unit]
+	select_sound.play()
