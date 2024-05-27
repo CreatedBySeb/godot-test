99e795114a09e1f6c7da871d50da31ae079ebf14
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2024-05-27 19:58:03 +0100

    Add unbounded movement

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -4,6 +4,7 @@ class_name GameManager
 @export var units: Array[Unit] = []
 
 @onready var camera: Camera = %Camera
+@onready var tilemap: TileMap = %TileMap
 @onready var select_sound = $SelectSound
 
 var selected_unit = 0
@@ -20,6 +21,14 @@ func _process(delta):
 		select_unit(1)
 	elif Input.is_action_just_pressed("select_prev_unit"):
 		select_unit(-1)
+
+	if Input.is_action_just_pressed("click"):
+		var mouse_pos = get_viewport().get_mouse_position()
+		var space_transform = camera.get_canvas_transform().affine_inverse()
+		var global_pos = space_transform * mouse_pos
+		var selected_tile = tilemap.local_to_map(tilemap.to_local(global_pos))
+
+		units[selected_unit].move_to_tile(selected_tile)
 
 
 func select_unit(direction: int):
--- scripts/Unit.gd
+++ scripts/Unit.gd
@@ -2,6 +2,10 @@ extends AnimatableBody2D
 class_name Unit
 
 @onready var game_manager: GameManager = %GameManager
+@onready var tilemap: TileMap = %TileMap
+
+var location = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,3 +16,8 @@ func _ready():
 func _process(delta):
 	var selected = game_manager.units[game_manager.selected_unit] == self
 	$SelectionIndicator.visible = selected
+
+
+func move_to_tile(tile: Vector2):
+	location = tile
+	position = tilemap.to_global(tilemap.map_to_local(tile))
